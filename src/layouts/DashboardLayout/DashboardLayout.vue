25030ba2db13f9918a497da7977c3712603f3c27
Sultan Mustafin <sultan.fb29@gmail.com>
2022-10-24 14:49:28 +0300

    feat: create application base layout and folder structure

---
+++ src/layouts/DashboardLayout/DashboardLayout.vue
@@ -0,0 +1,8 @@
+<script setup lang="ts">
+import DashboardHeader from "@/layouts/DashboardLayout/components/DashboardHeader/DashboardHeader.vue";
+</script>
+
+<template>
+  <DashboardHeader />
+  <RouterView />
+</template>
---
+++ src/layouts/DashboardLayout/components/DashboardHeader/DashboardHeader.vue
@@ -0,0 +1,10 @@
+<template>
+  <header class="header">This is header</header>
+</template>
+
+<style>
+.header {
+  width: 100vw;
+  height: 50px;
+}
+</style>
